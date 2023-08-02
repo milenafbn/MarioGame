4ff0cdb0baa005809642430f4f1580986fccc757
Milena Neves <80131766+milenafbn@users.noreply.github.com>
2023-08-01 23:31:23 -0300

    Alteração em Platform
    permitir a criação de mais plataformas

--- mario-game/index.js
+++ mario-game/index.js
@@ -40,10 +40,10 @@ class Player{
 }
 
 class Platform{
-    constructor(){
+    constructor({x,y}){ // ({ }) é um objeto, não argumentos tipo x,y apenas
         this.position = {
-            x: 200,
-            y: 100
+            x,  // = x: x,
+            y  // = y: y
         }
 
         this.width = 200
@@ -51,13 +51,15 @@ class Platform{
     }
 
     draw() {
-        c.fillStyle = 'red'
+        c.fillStyle = 'green'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 }
 
 const player = new Player()
-const platform = new Platform()
+const platforms = [new Platform({x:200, y:100}), new Platform({x: 500, y:200})]
+
+
 const keys = {
     right: {
         pressed: false
@@ -71,7 +73,9 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height) //limpa toda a tela
     player.update()
-    platform.draw()
+    platforms.forEach((platform) => {
+        platform.draw()
+    })
 
     if (keys.right.pressed && player.position.x < 400){
         player.velocity.x = 5
@@ -81,22 +85,26 @@ function animate() {
         player.velocity.x = 0
 
         if (keys.right.pressed) {
-            platform.position.x -= 5
-        }
-        if (keys.left.pressed) {
-            platform.position.x += 5
+            platforms.forEach((platform) => {
+                platform.position.x -= 5
+            })
+        } else if (keys.left.pressed) {
+            platforms.forEach((platform) => {
+                platform.position.x += 5
+            })
         }
     }
     //platform collision detection
-    if (player.position.y + player.height <= platform.position.y 
-    && player.position.y + player.height + player.velocity.y 
-    >= platform.position.y && player.position.x + player.width
-    >= platform.position.x && player.position.x 
-    <= platform.position.x + platform.width) {
-        player.velocity.y = 0
-    }
-    
-}
+    platforms.forEach((platform) => {
+        if (player.position.y + player.height <= platform.position.y 
+        && player.position.y + player.height + player.velocity.y 
+        >= platform.position.y && player.position.x + player.width
+        >= platform.position.x && player.position.x 
+        <= platform.position.x + platform.width) {
+            player.velocity.y = 0
+        }
+    })
+}           
 
 animate()
 
